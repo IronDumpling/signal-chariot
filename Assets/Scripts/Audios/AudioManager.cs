82ad69cc4e76953b2eda5e45c2189dc8ff848e68
Chuyue Zhang <irondumpling233@outlook.com>
2024-09-19 20:17:49 -0400

    separate audio groups

--- Assets/Scripts/Audios/AudioManager.cs
+++ Assets/Scripts/Audios/AudioManager.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 
 using SetUps;
 using Utils.Common;
+using System;
 
 namespace Audios
 {
     public class AudioManager : MonoSingleton<AudioManager>
     {
-        [SerializeField] private AudioSource m_source;
+        [SerializeField] private AudioSource m_androidSource; // board, module, mod pickup, android damage
+        [SerializeField] private AudioSource m_enemySource; // enemy damage
+        [SerializeField] private AudioSource m_bulletSource; // explosion, weapon fire
         [SerializeField] private SetUp m_setUp;
         private AudioSetUp[] m_soundEffects;
 
@@ -20,16 +23,33 @@ namespace Audios
         public void PlaySound(string soundName)
         {
             AudioSetUp sound = GetSoundByName(soundName);
+
+            AudioSource source = GetMixerGroup(sound.group);
+
             if (sound != null)
             {
-                m_source.volume = sound.volume;
-                m_source.pitch = sound.pitch;
-                m_source.clip = sound.clip;
-                // m_source.PlayOneShot(sound.clip);
-                m_source.Play();
+                source.volume = sound.volume;
+                source.pitch = sound.pitch;
+                source.clip = sound.clip;
+                source.Play();
             }
             else
                 Debug.LogWarning($"Sound: {soundName} not found!");
+        }
+
+        private AudioSource GetMixerGroup(AudioMixerGroup group)
+        {
+            switch (group)
+            {
+                case AudioMixerGroup.Android:
+                    return m_androidSource;
+                case AudioMixerGroup.Enemy:
+                    return m_enemySource;
+                case AudioMixerGroup.Bullet:
+                    return m_bulletSource;
+                default:
+                    return null;
+            }
         }
 
         private AudioSetUp GetSoundByName(string name)
--- Assets/Scripts/SetUps/AudioSetUp.cs
+++ Assets/Scripts/SetUps/AudioSetUp.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 namespace SetUps
 {
+    public enum AudioMixerGroup
+    {
+        Android,
+        Enemy,
+        Bullet,
+    }
+
     [Serializable]
     public class AudioSetUp
     {
         public string soundName;
+        public AudioMixerGroup group;
         public AudioClip clip;
         public float volume = 1f;
         public float pitch = 1f;
